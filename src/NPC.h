eb17f54b833431302eea6fa66bb9c5ff9e7e2dce
DangeL <92173667+DangeL187@users.noreply.github.com>
2022-06-02 15:55:09 +0300

    Delete src directory

--- src/NPC.h
+++
@@ -1,6 +0,0 @@
-if (NAME == "Trader") {
-    sprite = NPCTraderSprite;
-    dialog.push_back("I'm gay!");
-    dialog.push_back("I'm not kidding!");
-    dialog.push_back("And you?");
-}
--- src/spells_set.h
+++
@@ -1,8 +0,0 @@
-for (int i = 0; i < 27; i++) {
-    if (inv_spells[i] == 1) { //Cold Snap
-        SpellsInventoryPageSprite[i] = ColdSnapSprite;
-    }
-    else if (inv_spells[i] == 2) { //
-        //
-    }
-}
