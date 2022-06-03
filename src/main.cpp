e2df4458ce8824ab957d0f196bab1bfb3cd65c73
DangeL <92173667+DangeL187@users.noreply.github.com>
2022-06-03 16:38:52 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -10,99 +10,33 @@
 #include <cmath>
 #include <memory>
 #include "split.h"
-// #include "map.h"
-
 using namespace sf;
+#include "Map/Map.h"
+#include "Graphics/NewSprite.h"
+#include "Game/Game.h"
+#include "Map/DefineTile.h"
+#include "Game/WindowManager.h"
+#include "Game/Renderer.h"
 
-class NewSprite {
-public:
-    Texture texture;
-    Sprite sprite;
-
-    NewSprite() {
-        texture.loadFromFile("../images/map.png");
-        sprite.setTexture(texture);
-        sprite.setPosition(300, 300);
-    }
-
-    Sprite getSprite() {
-        return sprite;
-    }
-};
-
-class Game {
-protected:
-    View view;
-    unsigned int window_w = 1920;
-    unsigned int window_h = 1080;
-    std::shared_ptr<RenderWindow> window;
-    Clock clock;
-    float time;
-    std::pair<float, float> playerPosition{300.f, 300.f};
-    float player_x = 300;
-    float player_y = 300;
-public:
-
-};
-
-class Renderer: public Game {
-public:
-    std::vector<Sprite> v_sprites;
-
-    Renderer() = default;
-
-    void createWindow() {
-        window = std::make_shared<RenderWindow>(VideoMode(window_w, window_h), "Lord World");
-    }
-    void viewReset() {
-        view.reset(FloatRect(0, 0, window_w, window_h));
-    }
-
-    void windowHandleEvents() {
-        Event event;
-        while (window->pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window->close();
-            }
-        }
-    }
-    void windowSetView() {
-        window->setView(view);
-    }
-    void windowClear() {
-        window->clear();
-    }
-    void windowDisplay() {
-        window->display();
-    }
-    bool windowIsOpen() {
-        return window->isOpen();
-    }
-
-    void createSprite(std::string name) {
-        NewSprite *new_sprite = new NewSprite();
-        v_sprites.push_back(new_sprite->getSprite());
-    }
-    void drawSprites() {
-        for (auto& i_sprite : v_sprites) {
-            window->draw(i_sprite);
-        }
-    }
-};
-
-int main()
-{
+int main() {
+    Game game;
     Renderer renderer;
 
     renderer.createWindow();
     renderer.viewReset();
 
-    renderer.createSprite("map.png");
+    //renderer.createSprite("map.png");
+    renderer.createMapSprite("map.png");
 
     while(renderer.windowIsOpen()) {
         renderer.windowHandleEvents();
         renderer.windowSetView();
         renderer.windowClear();
+
+        //temp:
+        renderer.viewSetCenter();
+        //
+
         renderer.drawSprites();
         renderer.windowDisplay();
     }
