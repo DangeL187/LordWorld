cc838d5ab08f8f5cf1a27b57934b7cdd973768aa
DangeL <92173667+DangeL187@users.noreply.github.com>
2022-06-03 11:54:31 +0300

    Better OOP

--- src/LW.cpp
+++ src/LW.cpp
@@ -8,48 +8,68 @@
 #include <string>
 #include <sstream>
 #include <cmath>
+#include <memory>
 #include "split.h"
+#include "map.h"
 using namespace sf;
 
 class Game {
-public:
+private:
     View view;
-    Clock clock;
     unsigned int window_w = 1920;
     unsigned int window_h = 1080;
+protected:
+    Clock clock;
     float time;
     float player_x = 300;
     float player_y = 300;
-
+    std::shared_ptr<RenderWindow> window;
+public:
     Game() {
-        RenderWindow window(VideoMode(window_w, window_h), "Lord World");
+        window = std::make_shared<RenderWindow>(VideoMode(window_w, window_h), "Lord World");
         viewReset();
-
-        while(window.isOpen()) {
-            windowEventClosed(&window);
-            
-            window.setView(view);
-            window.clear();
-            window.display();
-        }
-    }
-
-    void windowEventClosed(auto get_window) {
-        Event event;
-        while (get_window->pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                get_window->close();
-            }
-        }
     }
 
     void viewReset() {
         view.reset(FloatRect(0, 0, window_w, window_h));
     }
+    bool windowIsOpen() {
+        return window->isOpen();
+    }
+    void windowEventClosed() {
+        Event event;
+        while (window->pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window->close();
+            }
+        }
+    }
+    void windowSetView() {
+        window->setView(view);
+    }
+    void windowClear() {
+        window->clear();
+    }
+    void windowDisplay() {
+        window->display();
+    }
+};
+
+class Renderer: public Game {
+    Renderer() {
+
+    }
 };
 
 int main() {
     Game game;
+
+    while(game.windowIsOpen()) {
+        game.windowEventClosed();
+        game.windowSetView();
+        game.windowClear();
+        game.windowDisplay();
+    }
 
     return 0;
 }
