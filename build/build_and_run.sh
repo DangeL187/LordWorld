bb9d560355e26dd8dd1e0aac0f31650ab8512020
DangeL <92173667+DangeL187@users.noreply.github.com>
2023-09-30 02:39:03 +0300

    Add files via upload

--- build/build_and_run.sh
+++ build/build_and_run.sh
@@ -1,1 +1,2 @@
-g++ ../src/main.cpp -o ../bin/LW -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system -static-libstdc++ -static-libgcc -lpthread -fpermissive && ../bin/LW
+g++ ../src/BuffListLoader.cpp -o ../bin/BuffListLoader && ../bin/BuffListLoader
+g++ ../src/main.cpp -o ../bin/LW -std=c++20 -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-system -static-libstdc++ -static-libgcc -lpthread -fpermissive && ../bin/LW
