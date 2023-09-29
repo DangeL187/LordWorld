f200300232a3ff4b176132d69f05c8dedc4b2621
DangeL <92173667+DangeL187@users.noreply.github.com>
2023-09-30 02:35:39 +0300

    Add files via upload

--- src/Game/Spells/Spells.h
+++ src/Game/Spells/Spells.h
@@ -1,13 +1,13 @@
 void setImages(auto& game, int size, auto& arr1, auto& arr2) {
     for (int i = 0; i < size; i++) {
         if (arr1[i] == 1) { //Cold Snap
-            arr2[i] = game.renderer->ColdSnapSprite;
+            arr2[i] = game.renderer->sprite_manager->ColdSnapSprite;
         }
         else if (arr1[i] == 2) { //Cold Snap2
-            arr2[i] = game.renderer->ColdSnap2Sprite;
+            arr2[i] = game.renderer->sprite_manager->ColdSnap2Sprite;
         }
         else {
-            arr2[i] = game.renderer->InventoryItemEmptySprite;
+            arr2[i] = game.renderer->sprite_manager->InventoryItemEmptySprite;
         }
     }
 }
@@ -32,15 +32,15 @@ void spells(auto& time, auto& game) {
         defineSpell(game.player, game.player->spell_name, damage_from_spell, mana_cost);
         game.spellDamaged(game);
         for (int v = 0; v < game.damaged_numbers.size(); v++) {
-            float mx = game.entity_manager->v_monsters[game.damaged_numbers[v]].getX();
-            float my = game.entity_manager->v_monsters[game.damaged_numbers[v]].getY();
+            auto& monster = game.entity_manager->v_monsters[game.damaged_numbers[v]];
+            float mx = monster.getX();
+            float my = monster.getY();
             float condx = pow(pow((mx - game.player->getX()), 2), 0.5);
             float condy = pow(pow((my - game.player->getY()), 2), 0.5);
             if (condx <= 300 && condy <= 300 && game.player->mp >= mana_cost) {
                 std::cout << "FUCK " << damage_from_spell << "\n";
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].giveBuff("ColdSnap", 5000);
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].hitMonster(damage_from_spell, time, game.player);
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].timer_ColdSnap_tick = 1000;
+                monster.giveBuff(game.player->spell_name, 5000);
+                monster.hitMonster(damage_from_spell, game);
                 break;
             }
         }
@@ -48,24 +48,25 @@ void spells(auto& time, auto& game) {
             game.player->cooldowns[game.player->spell_slot] = 15000;
             game.player->mp -= mana_cost;
         } else {
-            game.renderer->GuiNoManaSprites[game.player->spell_slot].setColor(sf::Color(0, 0, 0, 160));
+            game.renderer->sprite_manager->GuiNoManaSprites[game.player->spell_slot].setColor(sf::Color(0, 0, 0, 160));
             std::cout << "No mana!\n";
         }
     }
     if (game.player->spell_name == 2 && game.player->cooldowns[game.player->spell_slot] == 0) {
-        int damage_from_spell = 2 * game.player->magic_ice;
-        int mana_cost = 5;
+        int damage_from_spell;
+        int mana_cost;
+        defineSpell(game.player, game.player->spell_name, damage_from_spell, mana_cost);
         game.spellDamaged(game);
         for (int v = 0; v < game.damaged_numbers.size(); v++) {
-            float mx = game.entity_manager->v_monsters[game.damaged_numbers[v]].getX();
-            float my = game.entity_manager->v_monsters[game.damaged_numbers[v]].getY();
+            auto& monster = game.entity_manager->v_monsters[game.damaged_numbers[v]];
+            float mx = monster.getX();
+            float my = monster.getY();
             float condx = pow(pow((mx - game.player->getX()), 2), 0.5);
             float condy = pow(pow((my - game.player->getY()), 2), 0.5);
             if (condx <= 300 && condy <= 300 && game.player->mp >= mana_cost) {
                 std::cout << "FUCK " << damage_from_spell << "\n";
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].giveBuff("ColdSnap", 5000);
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].hitMonster(damage_from_spell, time, game.player);
-                game.entity_manager->v_monsters[game.damaged_numbers[v]].timer_ColdSnap_tick = 1000;
+                monster.giveBuff(game.player->spell_name-1, 5000);
+                monster.hitMonster(damage_from_spell, game);
                 break;
             }
         }
@@ -73,6 +74,7 @@ void spells(auto& time, auto& game) {
             game.player->cooldowns[game.player->spell_slot] = 15000;
             game.player->mp -= mana_cost;
         } else {
+            game.renderer->sprite_manager->GuiNoManaSprites[game.player->spell_slot].setColor(sf::Color(0, 0, 0, 160));
             std::cout << "No mana!\n";
         }
     }
