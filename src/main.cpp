eb5affa28aa1e40fe554d88cce8bf291fcff6ca7
DangeL <92173667+DangeL187@users.noreply.github.com>
2023-09-30 02:37:25 +0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,20 @@
 #include <cmath>
 #include <memory>
 #include <functional>
+#include <variant>
 #include "split.h"
 #define PI 3.14159265
 using namespace sf;
-#include "Graphics/NewSprite.h"
-#include "Graphics/DynamicText.h"
-#include "Map/Map.h"
 #include "Game/Game.h"
 
 int main() {
     Game game;
 
-    game.createWindow("Lord World", 1920, 1080);
+    game.createWindowManager("Lord World", 1920, 1080);
     game.createMap();
     game.createRenderer();
-    game.createSpriteLoader();
     game.createEntityManager();
+    game.createUpdateManager();
     game.window_manager->viewReset();
     game.createPlayer(500, 500, 44.0, 64.0);
     game.entity_manager->createMonster(300, 300, 50.0, 62.0, 1, game);
@@ -32,17 +30,14 @@ int main() {
     game.createNPC(800, 804, 44.0, 64.0, "Trader");
     game.entity_manager->createItem(600, 600, 1, game);
     game.entity_manager->createItem(700, 600, 2, game);
-    //game.AnimationWeaponSprite = game.AnimationWoodenSwordSprite;
-    game.renderer->AnimationWeaponSprite.setTextureRect(IntRect(0, 0, 1, 1));
 
     while(game.window_manager->windowIsOpen()) {
         //game.createMonster(400, 400, 50.0, 62.0, "Rat"); //stress test
         game.window_manager->windowHandleEvents();
         game.window_manager->windowSetView();
         game.window_manager->windowClear();
-
-        game.updates();
-        game.drawSprites();
+        game.update_manager->update(game);
+        game.renderer->sprite_manager->drawSprites(game);
         game.window_manager->windowDisplay();
     }
 
