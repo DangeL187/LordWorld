2cfd2394910a9d99e56cd91b39d6443f3128f031
DangeL <92173667+DangeL187@users.noreply.github.com>
2023-10-16 17:11:49 +0300

    Refactoring main

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,21 @@
 #include <memory>
 #include <functional>
 #include <variant>
+#include <numeric>
+#include <chrono>
+#include <thread>
 #include "split.h"
+#include "cordOperations.h"
+#include "Timer.h"
 #define PI 3.14159265
 using namespace sf;
+std::vector<float> fuck;
 #include "Game/Game.h"
 
 int main() {
     Game game;
 
-    game.createWindowManager("Lord World", 1920, 1080);
-    game.createMap();
-    game.createRenderer();
-    game.createEntityManager();
-    game.createUpdateManager();
-    game.window_manager->viewReset();
-    game.createPlayer(500, 500, 44.0, 64.0);
-    game.entity_manager->createMonster(300, 300, 50.0, 62.0, 1, game);
-    game.entity_manager->createMonster(400, 400, 50.0, 62.0, 2, game);
-    game.createNPC(800, 804, 44.0, 64.0, "Trader");
-    game.entity_manager->createItem(600, 600, 1, game);
-    game.entity_manager->createItem(700, 600, 2, game);
-
-    while(game.window_manager->windowIsOpen()) {
-        //game.createMonster(400, 400, 50.0, 62.0, "Rat"); //stress test
-        game.window_manager->windowHandleEvents();
-        game.window_manager->windowSetView();
-        game.window_manager->windowClear();
-        game.update_manager->update(game);
-        game.renderer->sprite_manager->drawSprites(game);
-        game.window_manager->windowDisplay();
-    }
+    game.run();
 
     return 0;
 }
